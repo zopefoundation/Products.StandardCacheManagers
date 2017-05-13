92a42a6d9016ac34d0af0e05c0380f3c4cbf1f33
Jens Vagelpohl <jens@netz.ooo>
2017-05-13 11:22:28 +0200

    - release prep for version 4.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.StandardCacheManagers',
-      version='4.0.dev0',
+      version='4.0.0',
       url='http://pypi.python.org/pypi/Products.StandardCacheManagers',
       license='ZPL 2.1',
       description="Cache managers for Zope 2.",
