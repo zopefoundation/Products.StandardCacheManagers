bd17354b8dd45022f085ac0cd801988acf976ed6
Jens Vagelpohl <jens@plyp.com>
2025-11-19 19:00:11 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def _read(fname):
 
 
 setup(name='Products.StandardCacheManagers',
-      version='6.0',
+      version='6.1.dev0',
       url='https://github.com/zopefoundation/Products.StandardCacheManagers',
       project_urls={
           'Issue Tracker': ('https://github.com/zopefoundation'
