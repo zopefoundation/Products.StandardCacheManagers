70548e2b5b6708db388582157db4f414d81b0f7b
Jens Vagelpohl <jens@plyp.com>
2025-11-19 18:53:44 +0100

    - prepare release 6.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def _read(fname):
 
 
 setup(name='Products.StandardCacheManagers',
-      version='6.0.dev0',
+      version='6.0',
       url='https://github.com/zopefoundation/Products.StandardCacheManagers',
       project_urls={
           'Issue Tracker': ('https://github.com/zopefoundation'
